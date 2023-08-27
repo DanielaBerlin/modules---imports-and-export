6fdc9f7100baa64479a9da19ba31d0cd1a9be19a
Daniela <danielacardona2203@gmail.com>
2023-08-27 23:38:42 +0200

    Refactor variable and function imports

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-import { num1, num2 } from "./js/variable.js";
+//Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
 
-console.log(num1);
-console.log(num2);
+import * as v from "./js/variable.js";
+import { add } from "./js/functions.js";
+
+console.log(v.num1);
+console.log(v.num2);
+
+// add(v.num1, v.num2);
+
+console.log(add(v.num1, v.num2));
